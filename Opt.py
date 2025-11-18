895ab0c1148461dda2fd32100271e420a366fb74
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-11-18 09:26:43 -0500

    Create Opt.py

---
+++ Opt.py
@@ -0,0 +1,22 @@
+def acc(x,y):
+    email = x
+    password = y
+    valid == false
+    char = 0
+
+    for i in x:
+        if "@" not in x: 
+            print ("You need to have an @ symbol")
+        elif "@" in x:
+            valid == true
+
+    if valid == true:
+        print:("What is the password?") 
+
+    for i in y:
+        str.isdigit 
+
+
+        len(y)
+        acc(Jayden@415, 1234567)    
+    
