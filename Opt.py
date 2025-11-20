d5fb3d627c3d18b35f31f5eb5cc9b3209f31041f
Jaydenl6767 <Jaydenl321@nycstudents.net>
2025-11-20 09:26:40 -0500

    Update Opt.py

--- Opt.py
+++ Opt.py
@@ -2,23 +2,31 @@ def acc(x,y):
     email = x
     password = y
     valid = False
-    char = 0
+    val = False
 
     if "@" not in x: 
-            print ("You need to have an @ symbol")
+            print("You need to have an @ symbol")
+            valid = False
     if "@" in x:
+            print("Your Email is valid")
             valid = True
 
-    if valid == True:
-        print("What is the password?")
-
-    for i in y:
-       char = char + 1
-    if char < 8:
+    if len(password) < 8:
             print("Your password must be at least 8 characters long")
-    if str.isdigit(y) 
+    if not any(ch.isdigit() for ch in password):
+           print("Your passsword must include at least one number")
+
+    if not (ch.isuppper() for ch in password):
+           print("Your password must include one uppercase letter") 
+
+    if (len(password) > 8) and (ch.isdigit() for ch in password) and (ch.isupper() for ch in password):
+             print("Your Password is valid")
+             val = True
+
+    if val == True and valid == True:
+             print("Logged in Correctly")
 
 
       
-acc("Jayden415")    
+acc("Jayden415","Ilove234tacoa")    
     
